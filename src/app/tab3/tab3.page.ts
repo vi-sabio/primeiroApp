0a95be1abed4a1fb2d0cf3be62f34f2f71370746
vi-sabio <vinicios_sabio@hotmail.com>
2022-09-09 20:57:17 -0300

    tabs3

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab3',
@@ -6,7 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page {
+  
+  
+  constructor(private toastController: ToastController) {}
+  
+  async mostrarToast() {
+    const toast = await this.toastController.create({
+      message: 'Hello World!',
+      duration: 1500,
+    });
 
-  constructor() {}
+    await toast.present();
+  }
 
 }
